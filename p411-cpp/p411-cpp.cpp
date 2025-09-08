046cd5f7eb0dc9ede0c08c2931790d57be42b78e
isHardCoded <ishardcoded@gmail.com>
2025-09-08 16:02:26 +0300

    Создали класс Character

--- p411-cpp/p411-cpp.cpp
+++ p411-cpp/p411-cpp.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+class Character {
+	public:
+		std::string name;
+		int level;
+		double mana;
+		bool isAlive;
+};
+
 int main()
 {
-    
+	Character character;
+
+	character.name = "John";
+	character.level = 5;
+	character.mana = 10.5;
+	character.isAlive = true;
+
+	std::cout << "Name: " << character.name << std::endl;
+	std::cout << "Level: " << character.level << std::endl;
+	std::cout << "Mana: " << character.mana << std::endl;
+	std::cout << "Is alive: " << character.isAlive << std::endl;
 }
 
