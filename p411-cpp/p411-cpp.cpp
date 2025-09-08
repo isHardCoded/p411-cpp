6f61d187e5b049a8dd37331b428a36ba9476249e
isHardCoded <ishardcoded@gmail.com>
2025-09-08 16:59:18 +0300

    upd

--- p411-cpp/p411-cpp.cpp
+++ p411-cpp/p411-cpp.cpp
@@ -6,20 +6,35 @@ class Character {
 		int level;
 		double mana;
 		bool isAlive;
+
+		Character (std::string name, int level, double mana, bool isAlive) {
+			this->name = name;
+			this->level = level;
+			this->mana = mana;
+			this->isAlive = isAlive;
+		}
+
+		void show() {
+			std::cout << "Name: " << name << std::endl;
+			std::cout << "Level: " << level << std::endl;
+			std::cout << "Mana: " << mana << std::endl;
+			std::cout << "Is alive: " << isAlive << std::endl;
+			std::cout << std::endl;
+		}
 };
 
 int main()
 {
-	Character character;
+	Character* characters[] = {
+		new Character("John", 5, 10.5, true),
+		new Character("Tom", 4, 2.5, false),
+		new Character("Tim", 12, 32.5, true),
+		new Character("Jim", 7, 14.5, true),
+		new Character("Bob", 1, 1, true)
+	};
 
-	character.name = "John";
-	character.level = 5;
-	character.mana = 10.5;
-	character.isAlive = true;
-
-	std::cout << "Name: " << character.name << std::endl;
-	std::cout << "Level: " << character.level << std::endl;
-	std::cout << "Mana: " << character.mana << std::endl;
-	std::cout << "Is alive: " << character.isAlive << std::endl;
+	for (auto character : characters) {
+		character->show();
+	}
 }
 
