9ca98b125f37f8b7f4c92cf63e0d3fd8286f91c9
isHardCoded <ishardcoded@gmail.com>
2025-09-17 17:52:07 +0500

    Lib task

--- p411-cpp/p411-cpp.cpp
+++ p411-cpp/p411-cpp.cpp
@@ -1,74 +1,205 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-class Shape {
-	public:
-		virtual double area() = 0;
-		virtual double perimeter() = 0;
-};
-
-class Circle : public Shape {
-	private:
-		double radius;
-
-	public:
-		Circle(double r) : radius(r) {}
-
-		double area() override {
-			return 3.14 * radius * radius;
-		}
-
-		double perimeter() override {
-			return 2 * 3.14 * radius;
-		}
-};
-
-class Rectangle : public Shape {
-	private:
-		double width;
-		double length;
+class Book {
+	protected:
+		std::string title;
+		std::string author;
+		std::string ISBN;
+		bool available;
 
 	public:
-		Rectangle(double w, double l) : width(w), length(l) {}
+		Book(const std::string& t, const std::string& a, const std::string& i)
+			: title(t), author(a), ISBN(i), available(true) {}
 
-		double area() override {
-			return width * length;
+		bool checkAvailability() const {
+			return available;
 		}
 
-		double perimeter() override {
-			return (width + length) * 2;
+		void setAvailability(bool avail) {
+			available = avail;
+		}
+
+		virtual std::string str() const {
+			return "Book: " + title + ", author: " + author + ", ISBN: " + ISBN + ", available" + (available ? "Yes" : "No");
 		}
 };
 
-class Triangle : public Shape {
+class EBook : public Book {
 	private:
-		double side1;
-		double side2;
-		double base;
-		double height;
+		std::string fileFormat;
 
 	public:
-		Triangle(double b, double h, double s1, double s2) 
-			: base(b), height(h), side1(s1), side2(s2) {}
+		EBook(const std::string& t, const std::string& a, const std::string& i, const std::string format)
+			: Book(t, a, i), fileFormat(format) {}
 
-		double area() override {
-			return (base * height) / 2;
+		std::string getFileFormat() const {
+			return fileFormat;
 		}
-		double perimeter() override {
-			return side1 + side2 + base;
+
+		std::string str() const override {
+			return "Book: " + title + ", author: " + author + ", ISBN: " + ISBN + ", Format: " + fileFormat + ", available" + (available ? "Yes" : "No");
+		}
+};
+
+class PrintedBook : public Book {
+	private:
+		int pageCount;
+
+	public:
+		PrintedBook(
+			const std::string& t,
+			const std::string& a,
+			const std::string& i,
+			const std::string format,
+			int pc) : Book(t, a, i), pageCount(pc) {
+		}
+
+		int getPageCount() const {
+			return pageCount;
+		}
+
+		std::string str() const override {
+			return "Book: " + title + ", author: " + author + ", ISBN: " + ISBN + ", Page count: " + std::to_string(pageCount) + ", available" + (available ? "Yes" : "No");
+		}
+};
+
+class Reader {
+	private:
+		std::string name;
+		int reader_id;
+		std::vector<Book*> borrowedBooks;
+
+	public:
+		Reader(const std::string& n, int id) : name(n), reader_id(id) {}
+
+		void borrowBook(Book* book) {
+			if (book->checkAvailability()) {
+				borrowedBooks.push_back(book);
+				book->setAvailability(false);
+
+				std::cout << name << " borrowed" << book->str() << std::endl;
+			}
+
+			else {
+				std::cout << "Book is not available" << std::endl;
+			}
+		}
+
+		void returnBook(Book* book) {
+			auto it = find(borrowedBooks.begin(), borrowedBooks.end(), book);
+
+			if (it != borrowedBooks.end()) {
+				borrowedBooks.erase(it);
+				(*it)->setAvailability(true);
+				
+
+				std::cout << name << " returned" << std::endl;
+			}
+
+			else {
+				std::cout << (*it)->str() << " not borrowed by " << name << std::endl;
+			}
+		}
+
+		std::string str() const {
+			std::string result = "Reader: " + name + "\nBorrowed books:";
+
+			if (borrowedBooks.empty()) result += "None";
+
+			for (auto book : borrowedBooks) {
+				result += "\n - " + book->str();
+			}
+
+			return result;
+		}
+};
+
+class Librarian {
+	private:
+		std::string name;
+		int employee_id;
+
+	public:
+		Librarian(const std::string& n, int id) : name(n), employee_id(id) {}
+
+		void addBook(std::vector<Book*>& books, Book* book) {
+			books.push_back(book);
+			std::cout << "Librarian: " << name << " added book " << book->str() << std::endl;
+		}
+
+		void removeBook(std::vector<Book*>& books, Book* book) {
+			auto it = find(books.begin(), books.end(), book);
+
+			if (it != books.end()) {
+				books.erase(it);
+				std::cout << "Librarian " << name << " removed book: " << book->str() << std::endl;
+			}
+
+			else {
+				std::cout << "Book not found in library" << std::endl;
+			}
+		}
+
+		std::string str() const {
+			return "Librarian " + name;
+		}
+};
+
+class Library {
+	private:
+		std::vector<Book*> books;
+		std::vector<Reader> readers;
+		std::vector<Librarian> librarians;
+
+	public:
+		void registerReader(const Reader& reader) {
+			readers.push_back(reader);
+			std::cout << "Registered reader: " << reader.str() << std::endl;
+		}
+
+		void addLibrarian(const Librarian& librarian) {
+			librarians.push_back(librarian);
+			std::cout << "Added librarian: " << librarian.str() << std::endl;
+		}
+
+		std::vector<Book*>& getBooks() {
+			return books;
+		}
+
+		std::vector<Reader> getReaders() const {
+			return readers;
+		}
+
+		std::vector<Librarian> getLibrarian() const {
+			return librarians;
 		}
 };
 
 int main()
 {
-	Shape* shapes[] = {
-		new Circle(5),
-		new Rectangle(2, 4),
-		new Triangle(2, 3, 4, 5)
-	};
+	Library lib;
+	Librarian lib1("Josh", 1);
 
-	for (auto shape : shapes) {
-		std::cout << "Area: " << shape->area() << std::endl;
-		std::cout << "Perimeter: " << shape->perimeter() << std::endl;
-	}
+	lib.addLibrarian(lib1);
+	
+	Book* b1 = new EBook("Test1", "test1", "test1", "pdf");
+	Book* b2 = new PrintedBook("Test2", "test2", "test2", "printed", 500);
+	
+	lib1.addBook(lib.getBooks(), b1);
+	lib1.addBook(lib.getBooks(), b2);
+
+	lib1.removeBook(lib.getBooks(), b2);
+
+	Reader r1("John", 1);
+	lib.registerReader(r1);
+
+	r1.borrowBook(b1);
+	std::cout << r1.str() << std::endl;
+
+	r1.returnBook(b1);
+	std::cout << r1.str() << std::endl;
 }
 
