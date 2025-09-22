f8ae072718d0c7883c51b60f2f3ac4b004efcddf
ishardcoded <ishardcoded@gmail.com>
2025-09-22 16:33:44 +0500

    static

--- p411-cpp/p411-cpp.cpp
+++ p411-cpp/p411-cpp.cpp
@@ -1,40 +1,25 @@
 #include <iostream>
 
-class Character {
-	public:
-		std::string name;
-		int level;
-		double mana;
-		bool isAlive;
+// cast
 
-		Character (std::string name, int level, double mana, bool isAlive) {
-			this->name = name;
-			this->level = level;
-			this->mana = mana;
-			this->isAlive = isAlive;
-		}
-
-		void show() {
-			std::cout << "Name: " << name << std::endl;
-			std::cout << "Level: " << level << std::endl;
-			std::cout << "Mana: " << mana << std::endl;
-			std::cout << "Is alive: " << isAlive << std::endl;
-			std::cout << std::endl;
-		}
-};
+// static_cast
+class Base {};
+class Derived : public Base {};
 
 int main()
 {
-	Character* characters[] = {
-		new Character("John", 5, 10.5, true),
-		new Character("Tom", 4, 2.5, false),
-		new Character("Tim", 12, 32.5, true),
-		new Character("Jim", 7, 14.5, true),
-		new Character("Bob", 1, 1, true)
-	};
+	int i = 10;
+	double d = static_cast<double>(i);
 
-	for (auto character : characters) {
-		character->show();
-	}
+	int x = 5, y = 2;
+	double result = static_cast<double>(x) / y;
+
+	char c = 'A';
+	int ascii = static_cast<int>(c);
+
+	std::cout << result;
+
+	Base* b = new Derived();
+	Derived* d = static_cast<Derived*>(b);
 }
 
