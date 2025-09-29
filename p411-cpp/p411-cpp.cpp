f5cf5bcfb44566e3fe29d21bb0c95081fb49ddfe
ishardcoded <ishardcoded@gmail.com>
2025-09-29 18:24:00 +0500

    Отправили GET запрос на jsonplaceholder с помощью cpr

--- p411-cpp/p411-cpp.cpp
+++ p411-cpp/p411-cpp.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main()
 {
-	
+	cpr::Response response = cpr::Get(cpr::Url{"https://jsonplaceholder.org/posts"});
+
+	std::cout << response.text << std::endl;
 }
 
