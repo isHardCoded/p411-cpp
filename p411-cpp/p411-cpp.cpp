8954294cbeab443b806222eea22a763a47c2d965
isHardCoded <ishardcoded@gmail.com>
2025-10-01 15:13:43 +0300

    Отправка POST запроса

--- p411-cpp/p411-cpp.cpp
+++ p411-cpp/p411-cpp.cpp
@@ -3,8 +3,28 @@
 
 int main()
 {
-	cpr::Response response = cpr::Get(cpr::Url{"https://jsonplaceholder.org/posts"});
+	std::string json_data = R"(
+		{ 
+			"name": "user1", 
+			"firstname": "Tom", 
+			"lastname": "Doe",
+			"city": "Moscow",
+			"avatar": "https://dogtowndogtraining.com/wp-content/uploads/2012/06/300x300-061-e1340955308953.jpg"
+		}
+	)";
 
-	std::cout << response.text << std::endl;
+	cpr::Response response = cpr::Post(
+		cpr::Url { "https://6764432352b2a7619f5bfef7.mockapi.io/users" },
+		cpr::Body { json_data },
+		cpr::Header { { "Content-Type", "application/json" }}
+	);
+
+	if (response.status_code == 201) {
+		std::cout << "Post created. Server response: " << std::endl << response.text << std::endl;
+	}
+	else {
+		std::cout << "Error. Code: " << response.status_code << std::endl;
+		std::cout << "Text: " << response.text << std::endl;
+	}
 }
 
